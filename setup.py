44049961880e72a15aa3a5babcce60f5e5842741
ivanmontero <ivanspmontero@gmail.com>
2018-03-21 17:30:35 -0700

    edited setup.py for release 0.3.1

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='elephas',
-      version='0.3',
+      version='0.3.1',
       description='Deep learning on Spark with Keras',
-      url='http://github.com/maxpumperla/elephas',
-      download_url='https://github.com/maxpumperla/elephas/tarball/0.3',
+      url='https://github.com/ivanmontero/elephas',
+      download_url='https://github.com/ivanmontero/elephas/tarball/0.3.1',
       author='Max Pumperla',
       author_email='max.pumperla@googlemail.com',
       install_requires=['keras', 'hyperas', 'flask'],
